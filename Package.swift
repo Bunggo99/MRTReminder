55e58571ff73a7ffee75c8e508025be6a6e533d9
Ardli FW <ardli.wangsaatmaja@binus.ac.id>
2023-07-18 11:27:21 +0700

    initial commit

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MRTReminder",
-            dependencies: []),
+            dependencies: [],
+        path: "Sources"),
         .testTarget(
             name: "MRTReminderTests",
             dependencies: ["MRTReminder"]),
---
+++ Sources/Views/SwiftUIView.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Ardli Fadhillah on 18/07/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    public init() {}
+    
+    @available(iOS 13.0, *)
+    public var body: some View {
+        Text("This is from package manager!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    
+    @available(iOS 13.0, *)
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
